fbbf41612af5f4e3de9b3b9fd2089c0e18626cf8
rqroz <rqroz1@gmail.com>
2017-06-09 22:35:30 -0300

    coding finished, qDebug still lives in it

--- QtClientConsumer/plotterwidget.cpp
+++ QtClientConsumer/plotterwidget.cpp
@@ -49,30 +49,46 @@ void PlotterWidget::paintEvent(QPaintEvent *event){
     if(!this->data.empty()){
         QStringList tmp;
         QDateTime dt;
-        qint64 previous_x = 0, previous_y = height(), current_x, current_y, normalizedX;
-        int i = 0, j = 10;
+        int heightDivider = 2;
+        qint64 previous_x = 0, previous_y = height()/heightDivider, current_x, current_y;
+
+        qDebug() << "width: " << width();
         for (QStringList::iterator it = this->data.begin(); it != this->data.end(); ++it) {
             QString current = *it;
             tmp = current.split(" ");
             if(tmp.size() == 2){
                 dt = QDateTime::fromString(tmp.at(0), Qt::ISODate);
+                int timestamp = dt.toTime_t();
 
-                current_x = dt.currentMSecsSinceEpoch();
-                current_y = height() - tmp.at(1).toFloat();
-                if(it == this->data.begin()){
-                    normalizedX = current_x;
+                current_x = timestamp % width();
+                if(current_x <= previous_x){
+                    qDebug() << "on reset hd: " << heightDivider;
+                    qDebug() << "on reset p: (" << previous_x << ", " << previous_y << ")";
+                    qDebug() << "on reset c: (" << current_x << ", " << height()/heightDivider - tmp.at(1).toFloat() << ")\n";
+                    previous_x = 0;
+                    if(heightDivider == 2){
+                        previous_y *= 2;
+                        heightDivider = 1;
+                    }else{
+                        previous_y /= 2;
+                        heightDivider = 2;
+                        painter.drawRect(0, 0, width(), height());
+                    }
+                    qDebug() << "after proc hd: " << heightDivider;
+                    qDebug() << "after proc p: (" << previous_x << ", " << previous_y << ")";
+                    qDebug() << "after proc c: (" << current_x << ", " << height()/heightDivider - tmp.at(1).toFloat() << ")\n";
+
                 }
-                qDebug() << "dt: " << dt.toString("dd.MM.yyyy hh:mm:ss") << " - " << dt.currentMSecsSinceEpoch();
-                qDebug() << "p_x: " << (float) previous_x/normalizedX  << " | p_y: " << previous_y;
-                qDebug() << "c_x: " << (float) current_x/normalizedX  << " | c_y: " << current_y;
-                qDebug() << "a_x: " << current_x << " | n_x: " << normalizedX << "\n";
 
-                painter.drawLine(i, previous_y, j, current_y);
+                current_y = height()/heightDivider - tmp.at(1).toFloat();
+
+                qDebug() << "dt: " << dt.toString("dd.MM.yyyy hh:mm:ss") << " - " << dt.toTime_t();
+                qDebug() << "p_x: " << (float) previous_x  << " | p_y: " << previous_y;
+                qDebug() << "c_x: " << (float) current_x  << " | c_y: " << current_y << "\n";
+
+                painter.drawLine(previous_x, previous_y, current_x, current_y);
                 previous_x = current_x;
                 previous_y = current_y;
-
-                i = j;
-                j = j + 10;
             }
         }
     }
